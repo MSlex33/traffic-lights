b328c540ad8e0d4ccc6995eecd897a9247e56d33
Alex Popov <mslex.exe@gmail.com>
2020-10-14 14:45:47 +0300

    good

--- traffic lights/ViewController.swift
+++ traffic lights/ViewController.swift
@@ -9,17 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var toggleTextButton: UIButton!
     @IBOutlet var redView: UIView!
-    @IBOutlet var YelloyView: UIView!
+    @IBOutlet var yelloyView: UIView!
     @IBOutlet var greenView: UIView!
+    @IBOutlet var toggleTextButton: UIButton!
+    
+    var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.alpha = 0.3
+        yelloyView.alpha = 0.3
+        greenView.alpha = 0.3
+        
+        view.backgroundColor = .black
     }
 
     @IBAction func toggleTextButtonPress() {
+        toggleTextButton.setTitle("NEXT", for: .normal)
+        
+        count += 1
+        
+        if count > 3 {
+            count = 1
+        }
+        
+        switch count {
+        case 1:
+            redView.alpha = 1
+            greenView.alpha = 0.3
+            yelloyView.alpha = 0.3
+        case 2:
+            redView.alpha = 0.3
+            yelloyView.alpha = 1
+            greenView.alpha = 0.3
+        case 3:
+            greenView.alpha = 1
+            redView.alpha = 0.3
+            yelloyView.alpha = 0.3
+        default:
+            break
+        }
+        
     }
+    
 }
 
